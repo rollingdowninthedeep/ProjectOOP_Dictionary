123fc9821b33f2ff3779f993cc4be5f8e178b43e
Quenn <145013411+rollingdowninthedeep@users.noreply.github.com>
2023-10-10 21:34:31 +0700

    Quyen
    - add Study.java and Study.fxml
    - studyButton now can go to Study's screen

--- Dictionary/src/main/java/com/example/dictionary/DictionaryController.java
+++ Dictionary/src/main/java/com/example/dictionary/DictionaryController.java
@@ -1,14 +1,21 @@
 package com.example.dictionary;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class DictionaryController {
     @FXML
-    private Label welcomeText;
+    private AnchorPane mainContent;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void study(ActionEvent actionEvent) throws Exception {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Study.fxml"));
+        mainContent.getChildren().setAll(pane);
     }
 }
