e4fea83b9c6856cd5534dcaf597ab8dcea90b261
dtpthanh <22021167@vnu.edu.vn>
2023-10-22 23:02:04 +0700

    Thanhh
    thêm giao diện cho Study với Search
    sửa file DictionaryApplication.java chỗ FXML Loader để test giao diện Study
    sửa kích cỡ trong dictionary-view.fxml

--- Dictionary/src/main/java/com/example/dictionary/Study.java
+++ Dictionary/src/main/java/com/example/dictionary/Study.java
@@ -1,4 +1,41 @@
 package com.example.dictionary;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class Study {
+  @FXML
+  private Button searchButton;
+  @FXML
+  private Button translateButton;
+
+  @FXML
+  public void search() {
+    loadNewFXML("Search.fxml");
+  }
+
+  @FXML
+  public void translate() {
+    loadNewFXML("Translate.fxml");
+  }
+
+  private void loadNewFXML(String fileName) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource(fileName));
+      Parent root = loader.load();
+
+      // Tạo một stage mới
+      Stage stage = new Stage();
+      stage.setScene(new Scene(root));
+      stage.show();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
