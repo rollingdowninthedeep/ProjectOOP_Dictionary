41a03424eacc237c195543dcc4523386f6cefe8e
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-10-23 13:13:32 +0700

    hí

---
+++ Dictionary/src/main/java/com/example/dictionary/DictionaryController.java
@@ -0,0 +1,21 @@
+package com.example.dictionary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DictionaryController {
+    @FXML
+    private AnchorPane mainContent;
+
+    @FXML
+    public void study(ActionEvent actionEvent) throws Exception {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Study.fxml"));
+        mainContent.getChildren().setAll(pane);
+    }
+}
---
+++ Dictionary/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.dictionary {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.dictionary to javafx.fxml;
+    exports com.example.dictionary;
+}
