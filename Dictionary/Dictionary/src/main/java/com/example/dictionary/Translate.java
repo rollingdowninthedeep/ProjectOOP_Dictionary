85620ba45fbcc0ede0b132714c16a1fd529add70
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-10-25 13:37:52 +0700

    hi

---
+++ Dictionary/Dictionary/src/main/java/com/example/dictionary/Translate.java
@@ -0,0 +1,13 @@
+package com.example.dictionary;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Translate {
+    private Button translate;
+}
