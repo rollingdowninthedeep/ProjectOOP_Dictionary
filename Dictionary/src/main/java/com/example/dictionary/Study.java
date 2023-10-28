b914b8056a774d515533a3a17e54adf300a25506
Quenn <145013411+rollingdowninthedeep@users.noreply.github.com>
2023-10-29 00:16:02 +0700

    update

--- Dictionary/src/main/java/com/example/dictionary/Study.java
+++ Dictionary/src/main/java/com/example/dictionary/Study.java
@@ -2,40 +2,68 @@ package com.example.dictionary;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Study {
-  @FXML
-  private Button searchButton;
-  @FXML
-  private Button translateButton;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
-  @FXML
-  public void search() {
-    loadNewFXML("Search.fxml");
-  }
+public class Study implements Initializable {
+    @FXML
+    private AnchorPane scene;
+    @FXML
+    private StackPane contentArea;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Button translateButton;
+    @FXML
+    private Button backButton;
 
-  @FXML
-  public void translate() {
-    loadNewFXML("Translate.fxml");
-  }
-
-  private void loadNewFXML(String fileName) {
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource(fileName));
-      Parent root = loader.load();
-
-      // Tạo một stage mới
-      Stage stage = new Stage();
-      stage.setScene(new Scene(root));
-      stage.show();
-
-    } catch (Exception e) {
-      e.printStackTrace();
+    @FXML
+    public void search() {
+      try {
+        AnchorPane content = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Search.fxml")));
+        contentArea.getChildren().setAll(content);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
-  }
+
+    @FXML
+    public void translate() {
+      try {
+        AnchorPane content = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Translate.fxml")));
+        contentArea.getChildren().setAll(content);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    @FXML
+    public void back() {
+      try {
+        AnchorPane scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dictionary-view.fxml")));
+        this.scene.getChildren().setAll(scene);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            AnchorPane content = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Translate.fxml")));
+            contentArea.getChildren().setAll(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
