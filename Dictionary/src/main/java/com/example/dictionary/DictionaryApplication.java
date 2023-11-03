9894aba9b0d3fadcfed402aada275a7870b5edb3
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-11-03 15:19:07 +0700

    Add files via upload

--- Dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
+++ Dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -3,7 +3,9 @@ package com.example.dictionary;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,6 +16,8 @@ public class DictionaryApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
         stage.show();
     }
 
--- Dictionary/src/main/java/com/example/dictionary/DictionaryController.java
+++ Dictionary/src/main/java/com/example/dictionary/DictionaryController.java
@@ -1,12 +1,14 @@
 package com.example.dictionary;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import jdk.internal.access.JavaIOFileDescriptorAccess;
 
 import java.io.IOException;
 
@@ -24,5 +26,10 @@ public class DictionaryController {
     public void play(ActionEvent actionEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("Game.fxml"));
         mainContent.getChildren().setAll(pane);
+    }
+
+    @FXML
+    public void exit(ActionEvent actionEvent) {
+        Platform.exit();
     }
 }
