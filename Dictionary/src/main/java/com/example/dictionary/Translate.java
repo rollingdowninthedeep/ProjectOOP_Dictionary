c816fdbd43c91cb62dff6dc69b418a308069ced3
dtpthanh <22021167@vnu.edu.vn>
2023-11-07 22:17:01 +0700

    v

--- Dictionary/src/main/java/com/example/dictionary/Translate.java
+++ Dictionary/src/main/java/com/example/dictionary/Translate.java
@@ -1,12 +1,14 @@
 package com.example.dictionary;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
-public class Translate extends Study implements Initializable {
+public class Translate implements Initializable {
 
     @FXML
     private TextArea inputText;
@@ -15,7 +17,7 @@ public class Translate extends Study implements Initializable {
     private TextArea ouputText;
 
     @FXML
-    public void initialize() {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
     @FXML
