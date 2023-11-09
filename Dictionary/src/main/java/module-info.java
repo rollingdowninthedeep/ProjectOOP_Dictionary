cef559366b46fd9d69abffec4fa5041d2b857796
dtpthanh <22021167@vnu.edu.vn>
2023-11-09 12:03:01 +0700

    thanhh

--- Dictionary/src/main/java/module-info.java
+++ Dictionary/src/main/java/module-info.java
@@ -9,7 +9,6 @@ module com.example.dictionary {
     requires java.sql;
     requires java.google.speech.api;
     requires jlayer;
-    requires json;
 
     opens com.example.dictionary to javafx.fxml;
     exports com.example.dictionary;
