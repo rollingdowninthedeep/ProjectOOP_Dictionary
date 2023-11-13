196a68ab25c37a2bb59cf4ae9a0f3b27e1c59bff
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-11-13 18:39:29 +0700

    Add files via upload

---
+++ Dictionary/src/main/java/DictionaryApp/Controller/Search.java
@@ -0,0 +1,134 @@
+package DictionaryApp.Controller;
+
+import DictionaryApp.Feature.Word;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Search extends Study implements Initializable {
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private ListView<String> searchListView = new ListView<>();
+
+    @FXML
+    private TextField titleText;
+    @FXML
+    private Button contentText;
+    @FXML
+    private TextField targetText;
+    @FXML
+    private TextArea meaningText;
+
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private AnchorPane resultArea;
+    @FXML
+    private AnchorPane fixingArea;
+
+    @FXML
+    public void searchFieldAction() {
+        fixingArea.setVisible(false);
+        resultArea.setVisible(false);
+
+        String s = searchTextField.getText();
+        setList(s);
+        if (!list.isEmpty()) {
+            searchListView.getItems().clear();
+            searchListView.setItems(list);
+        } else {
+            searchListView.getItems().clear();
+            searchListView.setPlaceholder(new Label("No result!"));
+        }
+    }
+
+    @FXML
+    public void onMouseClickedAction() {
+        String word = searchListView.getSelectionModel().getSelectedItem();
+        if (word != null) {
+            Word selectedWord = wordList.getWordArrayList().get(wordList.searchWord(word));
+            titleText.setText(selectedWord.getOrigin());
+            contentText.setText(selectedWord.getMeaning());
+            resultArea.setVisible(true);
+            fixingArea.setVisible(false);
+        } else {
+            titleText.setText(null);
+            contentText.setText(null);
+            resultArea.setVisible(false);
+        }
+    }
+    @FXML
+    public void onSpeechAction() {
+        speechEngine.speak(titleText.getText());
+    }
+
+    @FXML
+    public void onAddAction() {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/Views/Addition.fxml"));
+            mainPane.getChildren().clear();
+            mainPane.getChildren().add(pane);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onUpdateAction() {
+        targetText.setText(titleText.getText());
+        targetText.setEditable(false);
+        meaningText.setText(contentText.getText());
+        resultArea.setVisible(false);
+        fixingArea.setVisible(true);
+    }
+
+    @FXML
+    public void onSaveAction() {
+        Word word = new Word(targetText.getText(), meaningText.getText());
+        wordList.updateWord(word);
+
+        fixingArea.setVisible(false);
+    }
+
+    @FXML
+    public void onDeleteAction() {
+        String word = searchListView.getSelectionModel().getSelectedItem();
+        wordList.deleteWord(word);
+        searchTextField.setText(null);
+        setList("");
+        searchListView.setItems(list);
+
+        resultArea.setVisible(false);
+    }
+
+    private ObservableList<String> list = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setList("");
+        searchListView.setItems(list);
+    }
+
+
+    public void setList(String keyWord) {
+        if (keyWord.isBlank()) {
+            ObservableList<String> list = FXCollections.observableArrayList();
+            for (Word word : wordList.getWordArrayList()) {
+                list.add(word.getOrigin());
+                if (list.size() == 30) break;
+            }
+            this.list = list;
+        } else {
+            this.list = wordList.lookUpWord(keyWord);
+        }
+    }
+}
---
+++ Dictionary/src/main/java/DictionaryApp/Feature/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package DictionaryApp.Feature;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public DatabaseConnection() {
+        String databaseName = "thanh";
+        String databaseUsername = "root";
+        String databasePassword = "Thanh@2k4";
+        String url = "jdbc:mysql://127.0.0.1:3306/" + databaseName;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUsername, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return databaseLink;
+    }
+}
