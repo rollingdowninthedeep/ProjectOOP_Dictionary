cb117de6eb4ba33c7142ca63c5cc705442217ed9
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-11-13 17:38:37 +0700

    truong

--- Dictionary/src/main/java/com/example/dictionary/DatabaseConnection.java
+++ Dictionary/src/main/java/com/example/dictionary/DatabaseConnection.java
@@ -7,10 +7,17 @@ public class DatabaseConnection {
     public Connection databaseLink;
 
     public DatabaseConnection() {
+<<<<<<< Updated upstream:Dictionary/src/main/java/com/example/dictionary/DatabaseConnection.java
         String databaseName = "dictionarydata";
         String databaseUsername = "root";
         String databasePassword = "Quyen@030204";
         String url = "jdbc:mysql://localhost:3306/" + databaseName;
+=======
+        String databaseName = "thanh";
+        String databaseUsername = "root";
+        String databasePassword = "Thanh@2k4";
+        String url = "jdbc:mysql://127.0.0.1:3306/" + databaseName;
+>>>>>>> Stashed changes:Dictionary/src/main/java/DictionaryApp/Feature/DatabaseConnection.java
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             databaseLink = DriverManager.getConnection(url, databaseUsername, databasePassword);
