fc9ec379b65d20144d0edfdbb6ba3f2c92316a9a
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-12-07 13:42:31 +0700

    Update Game1.java

--- Dictionary/src/main/java/DictionaryApp/Controller/Game1.java
+++ Dictionary/src/main/java/DictionaryApp/Controller/Game1.java
@@ -378,6 +378,9 @@ public class Game1 {
 
     @FXML
     public void back(ActionEvent actionEvent) throws IOException {
+        counter = 0;
+        correct = 0;
+        wrong = 0;
         try {
             AnchorPane content = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Views/MenuGame1.fxml")));
             game1.getChildren().clear();
